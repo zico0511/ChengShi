73a125e249a91423e5a8d14711690c69c347600d
zico0511 <a673390783@gmail.com>
2026-04-17 22:56:24 +0800

    Add files via upload

---
+++ firebase-config.js
@@ -0,0 +1,15 @@
+// firebase-config.js
+// 這是您網站的 Firebase 配置與初始化文件。
+
+const firebaseConfig = { 
+  apiKey: "AIzaSyAhEbf2AZGpJjXWkRMax3pbNBtiI-tECZ8", 
+  authDomain: "chengshi-504cd.firebaseapp.com", 
+  projectId: "chengshi-504cd", 
+  storageBucket: "chengshi-504cd.firebasestorage.app", 
+  messagingSenderId: "1024615843263", 
+  appId: "1:1024615843263:web:1ed432681dbe0a25d68d0a", 
+  measurementId: "G-9X9EZDPF05" 
+}; 
+
+// 註：代碼中已統一使用 CDN 方式引入 Firebase SDK，
+// 此文件僅提供配置資訊供各頁面讀取。
